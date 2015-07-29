0967c7d212980ab9080f78a8cca6f2f476734ca5
root <root@odoo.htm-mbs.com>
2015-07-29 18:02:29 -0400

    correct position

--- models/sale.py
+++ models/sale.py
@@ -6,8 +6,8 @@ class SaleOrder(osv.osv):
 
     def prepare_odoo_record_vals(self, cr, uid, job, record, storeview=False):
 	vals = super(SaleOrder, self).prepare_odoo_record_vals(cr, uid, job, record, storeview=storeview)
-	if record.get('user_data'):
+	if record.get('messages') record['messages'].get('user_data'):
 	    user_obj = self.pool.get('res.users')
-	    vals['user_id'] = user_obj.get_or_create_odoo_record(cr, uid, job, record)
+	    vals['user_id'] = user_obj.get_or_create_odoo_record(cr, uid, job, record['messages']['user_data'])
 
 	return vals
